1b61592195dab7c53b77685ed94f15d484d4a79b
Nabeel <ahmad.nab331@gmail.com>
2021-09-22 18:41:17 +0500

    revised css

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -124,25 +124,3 @@
 
 	</div>
 @stop
-
-@section('css_styles')
-<style>
-    .nav_color {
-        color: #919bae !important
-    }
-    .dropdown:hover .dropdown-menu {
-        display: block;
-        margin-top: 0; // remove the gap so it doesn't close
-    }
-    .nav_brand1 {
-        background: none;
-    }
-    .vertical-line {
-        border-left: 1px solid #919bae;
-        height: 50px;
-        position: relative;
-        left: 4px;
-        top: 12px;
-    }
-    </style>
-@stop
--- resources/views/campaign/campaign_accordion.blade.php
+++ resources/views/campaign/campaign_accordion.blade.php
@@ -4,25 +4,6 @@
 
 @section('css_styles')
 
-<style>
-    .nav_color {
-        color: #919bae !important
-    }
-    .dropdown:hover .dropdown-menu {
-        display: block;
-        margin-top: 0; // remove the gap so it doesn't close
-    }
-    .nav_brand1 {
-        background: none;
-    }
-    .vertical-line {
-        border-left: 1px solid #919bae;
-        height: 50px;
-        position: relative;
-        left: 4px;
-        top: 12px;
-    }
-    </style>
     <link rel="stylesheet" href="{{asset('custom_css/style.css')}}">
 
 @stop
